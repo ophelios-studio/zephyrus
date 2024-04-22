4bfc016647208f9eb6ee31af6e0649716d4094c4
dtucker <davidt2003@msn.com>
2024-04-22 14:28:07 -0400

    feat: added accounting support for money format

--- src/Zephyrus/Utilities/Formatters/NumericFormatter.php
+++ src/Zephyrus/Utilities/Formatters/NumericFormatter.php
@@ -1,46 +1,47 @@
 <?php namespace Zephyrus\Utilities\Formatters;
 
 use Locale;
+use NumberFormatter;
 use Zephyrus\Application\Configuration;
 
 trait NumericFormatter
 {
-    public static function percent(int|float|null $number, int $minDecimals = 2, int $maxDecimals = 4): string
+    public static function percent(int|float|null $number, int $minDecimals = 0, int $maxDecimals = 2): string
     {
         if (is_null($number)) {
             return "-";
         }
-        $formatter = new \NumberFormatter(Locale::getDefault(), \NumberFormatter::PERCENT);
-        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
-        $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
-        $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
-        $result = $formatter->format($number, \NumberFormatter::TYPE_DOUBLE);
+        $formatter = new NumberFormatter(Locale::getDefault(), NumberFormatter::PERCENT);
+        $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
+        $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
+        $formatter->setAttribute(NumberFormatter::ROUNDING_MODE, NumberFormatter::ROUND_HALFUP);
+        $result = $formatter->format($number, NumberFormatter::TYPE_DOUBLE);
         return $result === false ? "-" : $result;
     }
 
-    public static function money(int|float|null $amount, int $minDecimals = 2, int $maxDecimals = 2): string
+    public static function money(int|float|null $amount, bool $accounting = false, int $minDecimals = 2, int $maxDecimals = 2): string
     {
         if (is_null($amount)) {
             return "-";
         }
-        $formatter = new \NumberFormatter(Locale::getDefault(), \NumberFormatter::CURRENCY);
-        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
-        $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
-        $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
+        $formatter = new NumberFormatter(Locale::getDefault(), $accounting ? NumberFormatter::CURRENCY_ACCOUNTING : NumberFormatter::CURRENCY);
+        $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
+        $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
+        $formatter->setAttribute(NumberFormatter::ROUNDING_MODE, NumberFormatter::ROUND_HALFUP);
         $result = $formatter->formatCurrency(round($amount, $maxDecimals), Configuration::getLocale('currency'));
         return $result === false ? "-" : $result;
     }
 
-    public static function decimal(int|float|null $number, int $minDecimals = 2, int $maxDecimals = 4): string
+    public static function decimal(int|float|null $number, int $minDecimals = 2, int $maxDecimals = 2): string
     {
         if (is_null($number)) {
             return "-";
         }
-        $formatter = new \NumberFormatter(Locale::getDefault(), \NumberFormatter::DECIMAL);
-        $formatter->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
-        $formatter->setAttribute(\NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
-        $formatter->setAttribute(\NumberFormatter::ROUNDING_MODE, \NumberFormatter::ROUND_HALFUP);
-        $result = $formatter->format(round($number, $maxDecimals), \NumberFormatter::TYPE_DOUBLE);
+        $formatter = new NumberFormatter(Locale::getDefault(), NumberFormatter::DECIMAL);
+        $formatter->setAttribute(NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
+        $formatter->setAttribute(NumberFormatter::MIN_FRACTION_DIGITS, $minDecimals);
+        $formatter->setAttribute(NumberFormatter::ROUNDING_MODE, NumberFormatter::ROUND_HALFUP);
+        $result = $formatter->format(round($number, $maxDecimals), NumberFormatter::TYPE_DOUBLE);
         return $result === false ? "-" : $result;
     }
 }
