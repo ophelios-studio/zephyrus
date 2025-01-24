e2685a9a8480909aed2183e2d1babaa73ae46451
jordanrioux <jordan.riouxleclair@gmail.com>
2025-01-24 12:44:12 -0500

    fix: Handle accented characters

--- src/Zephyrus/Utilities/StringUtility.php
+++ src/Zephyrus/Utilities/StringUtility.php
@@ -7,8 +7,8 @@ class StringUtility
         if (is_null($string)) {
             return "";
         }
-        return (strlen($string) > $length)
-            ? substr($string, 0, $length) . "..."
+        return (mb_strlen($string) > $length)
+            ? mb_substr($string, 0, $length) . "..."
             : $string;
     }
 
